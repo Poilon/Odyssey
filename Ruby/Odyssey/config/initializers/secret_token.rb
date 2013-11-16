3b17ed0904f464771d450842b41c83f0d808cdec
Poilon <Poilon@MacBook-Pro-de-Poilon.local>
2013-11-16 20:18:23 +0100

    First rails checkout

---
+++ Ruby/Odyssey/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Odyssey::Application.config.secret_key_base = 'b93441289e5ac0e19967563d4b33eb32b935b6ee92b412345be5ece473ed13f11a5e0702557088ff2bfb93ac914da3852ad70ab9c3ebd091de05446a06824067'
